a64026d6852f66fea9e1ea3711b7b98cc9d2959e
MagicCl <41426271+MagicCl@users.noreply.github.com>
2018-07-19 15:35:45 +0530

    admin/language/english/system
    files are uploaded

--- activity.php
+++ activity.php
@@ -1,86 +1,15 @@
 <?php
-class ControllerSystemActivity extends Controller {
-    private $error = array();
+// Heading
+$_['heading_title']			= 'Activity Log';
 
-    public function index() {
-        $this->data = $this->load->language('system/activity');
+// Column
+$_['column_date_added']		= 'Date Added';
+$_['column_message']		= 'Message';
 
-        $this->document->setTitle($this->language->get('heading_title'));
+// Text
+$_['text_no_results']		= 'There is no activity to list.';
+$_['text_success']			= 'You have successfully modified activity log.';
+$_['text_clear']			= '[Activity Log] Activity log was cleared by %s.';
 
-        $url = $this->build->url(array(
-            'page'
-        ));
-
-        $this->data['breadcrumbs'] = array();
-
-        $this->data['breadcrumbs'][] = array(
-            'text' => $this->language->get('text_home'),
-            'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], 'SSL')
-        );
-
-        $this->data['breadcrumbs'][] = array(
-            'text' => $this->language->get('heading_title'),
-            'href' => $this->url->link('system/activity', 'token=' . $this->session->data['token'] . $url, 'SSL')
-        );
-
-        if (isset($this->request->get['page'])) {
-            $page = (int)$this->request->get['page'];
-        } else {
-            $page = 1;
-        }
-
-        $filter_data = array(
-            'start' => $this->config->get('config_limit_admin') * ($page - 1),
-            'limit' => $this->config->get('config_limit_admin')
-        );
-
-        $this->load->model('system/activity');
-
-        $this->data['activities'] = array();
-
-        $activities = $this->model_system_activity->getActivities($filter_data);
-
-        foreach ($activities as $activity) {
-            $this->data['activities'][] = array(
-                'date_added' => date($this->language->get('datetime_format_short'), strtotime($activity['date_added'])),
-                'message'    => $activity['message']
-            );
-        }
-
-        $pagination = new Pagination();
-        $pagination->total = $this->model_system_activity->getTotalActivities();
-        $pagination->page = $page;
-        $pagination->limit = $this->config->get('config_limit_admin');
-        $pagination->url = $this->url->link('system/activity', 'token=' . $this->session->data['token'] . '&page={page}', 'SSL');
-
-        $this->data['pagination'] = $pagination->render();
-
-        $this->data['clear'] = $this->url->link('system/activity/clear', 'token=' . $this->session->data['token'], 'SSL');
-
-        $this->data['success'] = $this->build->data('success', $this->session->data);
-        $this->data['error_warning'] = $this->build->data('warning', $this->session->data);
-
-        $this->data['header'] = $this->load->controller('common/header');
-        $this->data['footer'] = $this->load->controller('common/footer');
-
-        $this->response->setOutput($this->render('system/activity.tpl'));
-    }
-
-    public function clear() {
-        $this->load->language('system/activity');
-
-        if (!$this->user->hasPermission('modify', 'system/activity')) {
-            $this->session->data['warning'] = $this->language->get('error_permission');
-        } else {
-            $this->load->model('system/activity');
-
-            $this->model_system_activity->deleteActivities();
-
-            $this->model_system_activity->addActivity(sprintf($this->language->get('text_clear'), $this->user->getUsername()));
-
-            $this->session->data['success'] = $this->language->get('text_success');
-        }
-
-        $this->response->redirect($this->url->link('system/activity', 'token=' . $this->session->data['token'], 'SSL'));
-    }
-}
+// Error
+$_['error_permission']		= 'You do not have permission to modify activity log.';
--- setting.php
+++ setting.php
@@ -1,148 +1,89 @@
 <?php
-class ControllerSystemSetting extends Controller {
-    private $error = array();
+// Heading
+$_['heading_title']							= 'Settings';
 
-    public function index() {
-        $this->data = $this->load->language('system/setting');
+// Column
+$_['column_action']							= 'Action';
+$_['column_notify_admin']					= 'Notify Admin';
+$_['column_notify_customer']				= 'Notify Customer';
+$_['column_additional_notification_email']	= 'Additional Notification Emails';
 
-        $this->document->setTitle($this->language->get('heading_title'));
+// Entry
+$_['entry_name']							= 'Name';
+$_['entry_registered_name']					= 'Registered Name';
+$_['entry_address']							= 'Address';
+$_['entry_email']							= 'Email';
+$_['entry_telephone']						= 'Telephone';
+$_['entry_fax']								= 'Fax';
+$_['entry_title']							= 'Meta Title';
+$_['entry_meta_description']				= 'Meta Description';
+$_['entry_logo']							= 'Logo Link';
+$_['entry_icon']							= 'Favicon Link';
+$_['entry_limit_admin']						= 'Admin List Limit';
+$_['entry_limit_admin']						= 'Admin List Limit';
+$_['entry_limit_application']				= 'Website List Limit';
+$_['entry_admin_language']					= 'Admin Language';
+$_['entry_language']						= 'Website Language';
+$_['entry_forgotten_admin']					= 'Admin Forgotten Password';
+$_['entry_forgotten_application']			= 'Application Forgotten Password';
+$_['entry_registration']					= 'Allow Customer Registration';
+$_['entry_home']							= 'Home Page';
+$_['entry_currency']						= 'Functional Currency';
+$_['entry_financial_year']					= 'Financial Year End (DD/MM)';
+$_['entry_auto_update_currency']			= 'Auto Update Currency Daily';
+$_['entry_invoice_prefix']					= 'Invoice Prefix';
+$_['entry_invoice_void_days']				= 'Void Invoice After Due (days)';
+$_['entry_draft_status']					= 'Invoice Draft Status';
+$_['entry_overdue_status']					= 'Invoice Overdue Status';
+$_['entry_paid_status']						= 'Invoice Paid Status';
+$_['entry_pending_status']					= 'Invoice Pending Status';
+$_['entry_void_status']						= 'Invoice Void Status';
+$_['entry_default_overdue_status']			= 'Default Overdue Status';
+$_['entry_default_void_status']				= 'Default Void Status';
+$_['entry_recurring_invoice_days']			= 'Generate Invoice Before Due (days)';
+$_['entry_recurring_disable_days']			= 'Disable Recurring After Due (days)';
+$_['entry_recurring_default_status']		= 'Default Recurring Payment Status';
+$_['entry_mail_protocol']					= 'Mail Protocol';
+$_['entry_mail_parameter']					= 'Mail Parameter';
+$_['entry_smtp_hostname']					= 'SMTP Hostname';
+$_['entry_smtp_username']					= 'SMTP Username';
+$_['entry_smtp_password']					= 'SMTP Password';
+$_['entry_smtp_port']						= 'SMTP Port';
+$_['entry_smtp_timeout']					= 'SMTP Timeout';
+$_['entry_ssl']								= 'Use SSL';
+$_['entry_seo_url']							= 'Use SEO URLs';
+$_['entry_maintenance']						= 'Maintenance Mode';
+$_['entry_compression']						= 'Output Compression Level';
+$_['entry_cache']							= 'Caching Mechanism';
+$_['entry_error_display']					= 'Display Errors';
+$_['entry_error_log']						= 'Log Errors';
+$_['entry_error_filename']					= 'Error Log File Name';
+$_['entry_cron_user']						= 'System Cron User';
+$_['entry_google_analytics']				= 'Google Analytics Code';
 
-        $this->document->addScript('view/javascript/summernote/summernote.min.js');
-        $this->document->addStyle('view/javascript/summernote/summernote.css');
+// Tab
+$_['tab_general']							= 'General';
+$_['tab_website']							= 'Website';
+$_['tab_accounting']						= 'Accounting';
+$_['tab_billing']							= 'Billing';
+$_['tab_recurring']							= 'Recurring Payments';
+$_['tab_mail']								= 'Mail';
+$_['tab_server']							= 'Server';
 
-        $this->data['breadcrumbs'] = array();
+// Text
+$_['text_success']							= 'You have successfully modified settings.';
+$_['text_mail']								= 'Mail';
+$_['text_smtp']								= 'SMTP';
+$_['text_file']								= 'File';
+$_['text_apc']								= 'APC';
+$_['text_memcache']							= 'Memcache';
 
-        $this->data['breadcrumbs'][] = array(
-            'text' => $this->language->get('text_home'),
-            'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], 'SSL')
-        );
-
-        $this->data['breadcrumbs'][] = array(
-            'text' => $this->language->get('heading_title'),
-            'href' => $this->url->link('system/setting', 'token=' . $this->session->data['token'], 'SSL')
-        );
-
-        $this->load->model('system/setting');
-
-        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-            $this->model_system_setting->editSetting('config', $this->request->post);
-
-            $this->session->data['success'] = $this->language->get('text_success');
-        }
-
-        $this->data['action'] = $this->url->link('system/setting', 'token=' . $this->session->data['token'], 'SSL');
-        $this->data['cancel'] = $this->url->link('system/setting', 'token=' . $this->session->data['token'], 'SSL');
-
-        $this->data['success'] = $this->build->data('success', $this->session->data);
-
-        $this->data['error_warning'] = $this->build->data('warning', $this->error);
-        $this->data['error_config_name'] = $this->build->data('config_name', $this->error);
-        $this->data['error_config_registered_name'] = $this->build->data('config_registered_name', $this->error);
-        $this->data['error_config_email'] = $this->build->data('config_email', $this->error);
-        $this->data['error_config_title'] = $this->build->data('config_title', $this->error);
-        $this->data['error_config_limit_admin'] = $this->build->data('config_limit_admin', $this->error);
-        $this->data['error_config_limit_application'] = $this->build->data('config_limit_application', $this->error);
-        $this->data['error_config_error_filename'] = $this->build->data('config_error_filename', $this->error);
-
-        $setting = $this->model_system_setting->getSetting('config');
-
-        $this->data['config_name'] = $this->build->data('config_name', $this->request->post, $setting);
-        $this->data['config_registered_name'] = $this->build->data('config_registered_name', $this->request->post, $setting);
-        $this->data['config_address'] = $this->build->data('config_address', $this->request->post, $setting);
-        $this->data['config_email'] = $this->build->data('config_email', $this->request->post, $setting);
-        $this->data['config_telephone'] = $this->build->data('config_telephone', $this->request->post, $setting);
-        $this->data['config_fax'] = $this->build->data('config_fax', $this->request->post, $setting);
-        $this->data['config_title'] = $this->build->data('config_title', $this->request->post, $setting);
-        $this->data['config_meta_description'] = $this->build->data('config_meta_description', $this->request->post, $setting);
-        $this->data['config_logo'] = $this->build->data('config_logo', $this->request->post, $setting);
-        $this->data['config_icon'] = $this->build->data('config_icon', $this->request->post, $setting);
-        $this->data['config_limit_admin'] = $this->build->data('config_limit_admin', $this->request->post, $setting);
-        $this->data['config_limit_application'] = $this->build->data('config_limit_application', $this->request->post, $setting);
-        $this->data['config_admin_language'] = $this->build->data('config_admin_language', $this->request->post, $setting);
-        $this->data['config_language'] = $this->build->data('config_language', $this->request->post, $setting);
-        $this->data['config_forgotten_admin'] = $this->build->data('config_forgotten_admin', $this->request->post, $setting);
-        $this->data['config_forgotten_application'] = $this->build->data('config_forgotten_application', $this->request->post, $setting);
-        $this->data['config_registration'] = $this->build->data('config_registration', $this->request->post, $setting);
-        $this->data['config_home'] = $this->build->data('config_home', $this->request->post, $setting);
-        $this->data['config_currency'] = $this->build->data('config_currency', $this->request->post, $setting);
-        $this->data['config_financial_year'] = $this->build->data('config_financial_year', $this->request->post, $setting);
-        $this->data['config_auto_update_currency'] = $this->build->data('config_auto_update_currency', $this->request->post, $setting);
-        $this->data['config_invoice_prefix'] = $this->build->data('config_invoice_prefix', $this->request->post, $setting);
-        $this->data['config_invoice_void_days'] = $this->build->data('config_invoice_void_days', $this->request->post, $setting);
-        $this->data['config_draft_status'] = $this->build->data('config_draft_status', $this->request->post, $setting, array());
-        $this->data['config_overdue_status'] = $this->build->data('config_overdue_status', $this->request->post, $setting, array());
-        $this->data['config_paid_status'] = $this->build->data('config_paid_status', $this->request->post, $setting, array());
-        $this->data['config_pending_status'] = $this->build->data('config_pending_status', $this->request->post, $setting, array());
-        $this->data['config_void_status'] = $this->build->data('config_void_status', $this->request->post, $setting, array());
-        $this->data['config_default_overdue_status'] = $this->build->data('config_default_overdue_status', $this->request->post, $setting, array());
-        $this->data['config_default_void_status'] = $this->build->data('config_default_void_status', $this->request->post, $setting, array());
-        $this->data['config_recurring_invoice_days'] = $this->build->data('config_recurring_invoice_days', $this->request->post, $setting);
-        $this->data['config_recurring_disable_days'] = $this->build->data('config_recurring_disable_days', $this->request->post, $setting);
-        $this->data['config_recurring_default_status'] = $this->build->data('config_recurring_default_status', $this->request->post, $setting);
-        $this->data['config_mail'] = $this->build->data('config_mail', $this->request->post, $setting);
-        $this->data['config_mail_alert'] = $this->build->data('config_mail_alert', $this->request->post, $setting);
-        $this->data['config_ssl'] = $this->build->data('config_ssl', $this->request->post, $setting);
-        $this->data['config_seo_url'] = $this->build->data('config_seo_url', $this->request->post, $setting);
-        $this->data['config_maintenance'] = $this->build->data('config_maintenance', $this->request->post, $setting);
-        $this->data['config_compression'] = $this->build->data('config_compression', $this->request->post, $setting);
-        $this->data['config_cache'] = $this->build->data('config_cache', $this->request->post, $setting);
-        $this->data['config_error_display'] = $this->build->data('config_error_display', $this->request->post, $setting);
-        $this->data['config_error_log'] = $this->build->data('config_error_log', $this->request->post, $setting);
-        $this->data['config_error_filename'] = $this->build->data('config_error_filename', $this->request->post, $setting);
-        $this->data['config_cron_user_id'] = $this->build->data('config_cron_user_id', $this->request->post, $setting);
-        $this->data['config_google_analytics'] = $this->build->data('config_google_analytics', $this->request->post, $setting);
-
-        $this->load->model('system/language');
-
-        $this->data['languages'] = $this->model_system_language->getLanguages();
-
-        $this->load->model('accounting/currency');
-
-        $this->data['currencies'] = $this->model_accounting_currency->getCurrencies();
-
-        $this->load->model('accounting/account');
-
-        $this->data['accounts'] = $this->model_accounting_account->getAccounts();
-
-        $this->load->model('system/status');
-
-        $this->data['statuses'] = $this->model_system_status->getStatuses();
-
-        $this->load->model('system/user');
-
-        $this->data['users'] = $this->model_system_user->getUsers();
-
-        $this->data['header'] = $this->load->controller('common/header');
-        $this->data['footer'] = $this->load->controller('common/footer');
-
-        $this->response->setOutput($this->render('system/setting.tpl'));
-    }
-
-    protected function validate() {
-        if (!$this->user->hasPermission('modify', 'system/setting')) {
-            $this->error['warning'] = $this->language->get('error_permission');
-        }
-
-        if ((utf8_strlen($this->request->post['config_name']) < 3) || (utf8_strlen($this->request->post['config_title']) > 64)) {
-            $this->error['config_name'] = $this->language->get('error_config_name');
-        }
-
-        if ((utf8_strlen($this->request->post['config_registered_name']) < 3) || (utf8_strlen($this->request->post['config_registered_name']) > 64)) {
-            $this->error['config_registered_name'] = $this->language->get('error_config_registered_name');
-        }
-
-        if ((utf8_strlen($this->request->post['config_title']) < 3) || (utf8_strlen($this->request->post['config_title']) > 255)) {
-            $this->error['config_title'] = $this->language->get('error_config_title');
-        }
-
-        if ((utf8_strlen($this->request->post['config_email']) > 96) || !preg_match('/^[^\@]+@.*\.[a-z]{2,6}$/i', $this->request->post['config_email'])) {
-            $this->error['config_email'] = $this->language->get('error_config_email');
-        }
-
-        if (!$this->request->post['config_error_filename']) {
-            $this->error['config_error_filename'] = $this->language->get('error_config_error_filename');
-        }
-
-        return !$this->error;
-    }
-}
+// Error
+$_['error_permission']						= 'You do not have permission to modify settings.';
+$_['error_config_name']						= 'Name must be between 3 and 64 characters.';
+$_['error_config_registered_name']			= 'Registered name must be between 3 and 64 characters.';
+$_['error_config_email']					= 'Email address does not seem to be valid.';
+$_['error_config_title']					= 'Title must be between 3 and 255 characters.';
+$_['error_config_limit_admin']				= 'Admin list limit required.';
+$_['error_config_limit_application']		= 'Website list limit required.';
+$_['error_config_error_filename']			= 'Error log file name is required.';
