34e671719bfbcacd074ac8e2b8c1471fd6d60f2b
MagicCl <41426271+MagicCl@users.noreply.github.com>
2018-07-19 16:20:24 +0530

    application\language\english\common
    files are uploaded

--- footer.php
+++ footer.php
@@ -1,10 +1,2 @@
 <?php
-class ControllerCommonFooter extends Controller {
-    public function index() {
-        $this->load->language('common/footer');
-
-       
-
-        return $this->render('common/footer.tpl');
-    }
-}
+$_['text_powered'] 		= '%s &copy; %s.<br />Powered by <a href="http://www.logicinvoice.com">Logic Invoice</a>.';
