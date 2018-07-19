0f5fe200058e253dd07ebd2633b4104587b96269
MagicCl <41426271+MagicCl@users.noreply.github.com>
2018-07-19 15:34:38 +0530

    admin/language/english/report
    files are uploaded

--- invoice.php
+++ invoice.php
@@ -1,82 +1,25 @@
 <?php
 // Heading
-$_['heading_title']				= 'Invoices';
+$_['heading_title']				= 'Invoices Report';
 
 // Column
-$_['column_invoice_id']			= 'Invoice ID';
-$_['column_name']				= 'Name';
+$_['column_date_start']			= 'Date Start';
+$_['column_date_end']			= 'Date End';
+$_['column_total_invoices']		= 'Total Invoices';
+$_['column_total_items']		= 'Total Items';
+$_['column_total_tax']			= 'Total Tax';
 $_['column_total']				= 'Total';
-$_['column_status']				= 'Status';
-$_['column_date_issued']		= 'Date Issued';
-$_['column_date_due']			= 'Date Due';
-$_['column_date_modified']		= 'Last Modified';
-$_['column_journal_entry']		= 'Journal Entry';
-$_['column_action']				= 'Action';
-$_['column_number']				= 'No.';
-$_['column_title']				= 'Title';
-$_['column_description']		= 'Description';
-$_['column_quantity']		    = 'Quantity';
-$_['column_price']				= 'Price';
-$_['column_tax_class']			= 'Tax Class';
-$_['column_discount']			= 'Discount';
-$_['column_total']				= 'Total';
-$_['column_status']				= 'Status';
-$_['column_comment']			= 'Comment';
-$_['column_date_added']			= 'Date Added';
 
 // Entry
-$_['entry_invoice_id']			= 'Invoice ID';
-$_['entry_customer']			= 'Customer';
-$_['entry_firstname']			= 'First Name';
-$_['entry_lastname']			= 'Last Name';
-$_['entry_company']				= 'Company';
-$_['entry_website']				= 'Website';
-$_['entry_email']				= 'Email Address';
-$_['entry_payment_firstname']	= 'Payment First Name';
-$_['entry_payment_lastname']	= 'Payment Last Name';
-$_['entry_payment_company']		= 'Payment Company';
-$_['entry_payment_address_1']	= 'Payment Address 1';
-$_['entry_payment_address_2']	= 'Payment Address 2';
-$_['entry_payment_city']		= 'Payment City';
-$_['entry_payment_postcode']	= 'Payment Postcode';
-$_['entry_payment_country']		= 'Payment Country';
-$_['entry_payment_zone']		= 'Payment Zone';
-$_['entry_payment_code']		= 'Payment Method';
-$_['entry_payment_name']		= 'Payment Name';
-$_['entry_payment_description']	= 'Payment Description';
-$_['entry_currency_code']		= 'Currency';
-$_['entry_currency_value']		= 'Currency Value';
-$_['entry_comment']				= 'Comment';
+$_['entry_date_issued_start']	= 'Date Issued Start';
+$_['entry_date_issued_end']		= 'Date Issued End';
 $_['entry_status']				= 'Status';
-$_['entry_date_due']			= 'Date Due';
-
-// Tab
-$_['tab_general']				= '1. General';
-$_['tab_payment_detail']		= '2. Payment Details';
-$_['tab_payment_method']		= '3. Payment Method';
-$_['tab_item']					= '4. Items';
-$_['tab_total']					= '5. Review';
-$_['tab_history']				= '5. History';
+$_['entry_group_by']			= 'Group By';
 
 // Text
-$_['text_no_results']			= 'There is no invoice to list.';
-$_['text_success']				= 'You have successfully modified invoices.';
-$_['text_no_histories']			= 'There is no history to list.';
-$_['text_updated']				= 'Updated:';
-$_['text_issued']				= 'Issued:';
-$_['text_item']					= 'Items';
-$_['text_payment']				= 'Payment';
-
-// Tooltip
-$_['tooltip_price']             = 'Price per quantity, excluding taxes.';
-
-// Error
-$_['error_permission']			= 'You do not have permission to modify invoices.';
-$_['error_firstname']			= 'First Name must be between 1 and 32 characters.';
-$_['error_lastname']			= 'Last Name must be between 1 and 32 characters.';
-$_['error_email']	 			= 'Email address does not seem to be valid.';
-$_['error_payment_name']	 	= 'Payment name must not be empty.';
-$_['error_currency_code']	 	= 'Currency must be selected.';
-$_['error_currency_value']	 	= 'Currency value must be greater than 0.';
-$_['error_date_due']	 		= 'Date due must not be empty.';
-$_['error_item']	 			= 'At least one item is required for total calculation.';
+$_['text_no_results']			= 'There is no report to list.';
+$_['text_all_statuses']			= 'All Statuses';
+$_['text_year']					= 'Year';
+$_['text_month']				= 'Month';
+$_['text_week']					= 'Week';
+$_['text_day']					= 'Day';
--- recurring.php
+++ recurring.php
@@ -1,57 +1,26 @@
 <?php
 // Heading
-$_['heading_title']				= 'Recurring Payments';
+$_['heading_title']				= 'Recurring Payment Report';
 
 // Column
-$_['column_recurring_id']		= 'Recurring ID';
-$_['column_name']				= 'Name';
-$_['column_total']				= 'Total';
-$_['column_status']				= 'Status';
+$_['column_date_start']			= 'Date Start';
+$_['column_date_end']			= 'Date End';
 $_['column_cycle']				= 'Cycle';
-$_['column_date_added']			= 'Date Added';
-$_['column_date_due']			= 'Date Due';
-$_['column_date_modified']		= 'Last Modified';
-$_['column_action']				= 'Action';
-$_['column_number']				= 'No.';
-$_['column_title']				= 'Title';
-$_['column_description']		= 'Description';
-$_['column_quantity']			= 'Quantity';
-$_['column_price']				= 'Price';
-$_['column_tax_class']			= 'Tax Class';
-$_['column_discount']			= 'Discount';
+$_['column_total_recurrings']	= 'Total Recurring Payments';
+$_['column_total_items']		= 'Total Items';
+$_['column_total_tax']			= 'Total Tax';
 $_['column_total']				= 'Total';
 
 // Entry
-$_['entry_recurring_id']		= 'Recurring ID';
-$_['entry_customer']			= 'Customer';
-$_['entry_payment_code']		= 'Payment Method';
-$_['entry_payment_name']		= 'Payment Name';
-$_['entry_payment_description']	= 'Payment Description';
-$_['entry_currency_code']		= 'Currency';
-$_['entry_currency_value']		= 'Currency Value';
-$_['entry_comment']				= 'Comment';
+$_['entry_date_added_start']	= 'Date Added Start';
+$_['entry_date_added_end']		= 'Date Added End';
 $_['entry_status']				= 'Status';
-$_['entry_cycle']				= 'Cycle';
-$_['entry_date_due']			= 'Date Due';
-
-// Tab
-$_['tab_general']				= '1. General';
-$_['tab_payment_method']		= '2. Payment Method';
-$_['tab_item']					= '3. Items';
-$_['tab_total']					= '4. Totals';
+$_['entry_group_by']			= 'Group By';
 
 // Text
-$_['text_no_results']			= 'There is no recurring to list.';
-$_['text_success']				= 'You have successfully modified recurring payments.';
-
-// Tooltip
-$_['tooltip_price']				= 'Price per quantity, excluding taxes.';
-
-// Error
-$_['error_permission']			= 'You do not have permission to modify recurring payments.';
-$_['error_customer']			= 'Customer is required.';
-$_['error_payment_name']	 	= 'Payment name must not be empty.';
-$_['error_currency_code']	 	= 'Currency must be selected.';
-$_['error_currency_value']	 	= 'Currency value must be greater than 0.';
-$_['error_date_due']	 		= 'Date due must not be empty.';
-$_['error_item']	 			= 'At least one item is required for total calculation.';
+$_['text_no_results']			= 'There is no report to list.';
+$_['text_all_statuses']			= 'All Statuses';
+$_['text_year']					= 'Year';
+$_['text_month']				= 'Month';
+$_['text_week']					= 'Week';
+$_['text_day']					= 'Day';
