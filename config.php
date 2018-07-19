886c2137038cca09f480bf17eb8f596d0874915a
MagicCl <41426271+MagicCl@users.noreply.github.com>
2018-07-19 16:44:11 +0530

    Add files via upload

--- config.php
+++ config.php
@@ -1,31 +1,23 @@
 <?php
-class Config {
-    private $data = array();
+// HTTP
+define('HTTP_SERVER', 'http://localhost/invoice/');
 
-    public function get($key) {
-        return (isset($this->data[$key]) ? $this->data[$key] : null);
-    }
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/invoice/');
 
-    public function set($key, $value) {
-        $this->data[$key] = $value;
-    }
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\invoice/application/');
+define('DIR_CACHE', 'C:\wamp\www\invoice/system/cache/');
+define('DIR_SYSTEM', 'C:\wamp\www\invoice/system/');
+define('DIR_DATABASE', 'C:\wamp\www\invoice/system/library/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\invoice/application/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\invoice/application/view/template/');
+define('DIR_LOGS', 'C:\wamp\www\invoice/system/logs/');
 
-    public function has($key) {
-        return isset($this->data[$key]);
-    }
-
-    public function load($filename) {
-        $file = DIR_CONFIG . $filename . '.php';
-
-        if (file_exists($file)) {
-            $_ = array();
-
-            require($file);
-
-            $this->data = array_merge($this->data, $_);
-        } else {
-            trigger_error('Error: Could not load config ' . $filename . '!');
-            exit();
-        }
-    }
-}
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'hewa');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'invoice');
+define('DB_PREFIX', 'xvmwb_');
