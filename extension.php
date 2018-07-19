662d59bc2c30cf52612dc7f2644dc6d107d8f021
MagicCl <41426271+MagicCl@users.noreply.github.com>
2018-07-19 16:27:18 +0530

    application\model\extension

--- extension.php
+++ extension.php
@@ -17,16 +17,4 @@ class ModelExtensionExtension extends Model {
 
         return $extension_data;
     }
-
-    public function install($type, $code) {
-        $this->db->query("INSERT INTO " . DB_PREFIX . "extension SET `type` = '" . $this->db->escape($type) . "', `code` = '" . $this->db->escape($code) . "'");
-
-        $this->cache->delete('extension');
-    }
-
-    public function uninstall($type, $code) {
-        $this->db->query("DELETE FROM " . DB_PREFIX . "extension WHERE `type` = '" . $this->db->escape($type) . "' AND `code` = '" . $this->db->escape($code) . "'");
-
-        $this->cache->delete('extension');
-    }
 }
